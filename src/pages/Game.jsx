a1f8d5a98c6c624f715a4f2d2cb032401b74ad99
aathvikguru <aathvikguru02@gmail.com>
2025-12-11 10:07:40 +0530

    timer change

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -30,7 +30,7 @@ const Game = () => {
     switch (level) {
       case 1: return 60;
       case 2: return 45;
-      case 3: return 20;
+      case 3: return 30;
       default: return 0;
     }
   };
